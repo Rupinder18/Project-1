823c148e921b74a28025050e0dc6cf4e130697bf
Your name here <rupi2003nder@gmail.com>
2021-06-01 21:18:28 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
     <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="..."/>
+  <h1>hello</h1>
   <div className="card-body">
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
