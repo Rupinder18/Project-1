43616bbc01590ed23997f6a0f95636344ac7cb6d
Your name here <rupi2003nder@gmail.com>
2021-06-01 21:13:07 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="..."/>
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
     </div>
   );
 }
